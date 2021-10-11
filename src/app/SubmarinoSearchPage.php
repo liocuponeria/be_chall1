b464a3a22c1a7324627601d80575eaa2f5df5230
Arthur Faria <arthur.agf@gmail.com>
2021-10-10 21:14:01 -0300

    Preparing the environment

---
+++ src/app/SubmarinoSearchPage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\Page;
+
+class SubmarinoSearchPage extends Page
+{
+    
+}
---
+++ src/app/SubmarinoTvPage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\SubmarinoSearchPage;
+
+class SubmarinoTvPage extends SubmarinoSearchPAge
+{
+
+}
